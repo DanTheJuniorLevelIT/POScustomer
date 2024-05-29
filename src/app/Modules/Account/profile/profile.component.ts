77eefb851bfd64c5bf9f8d43394680e4e860ec38
Dan Malayo <danielmalayo2@gmail.com>
2024-05-29 21:08:56 +0800

    next commit

--- src/app/Modules/Account/profile/profile.component.ts
+++ src/app/Modules/Account/profile/profile.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { CustomerService } from '../../../customer.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -15,26 +16,32 @@ export class ProfileComponent implements OnInit{
 user:any;
 customer:any;
 userID: any;
+selectedFile: any;
+filename: any;
 
 uploadedImage: string | ArrayBuffer | null = null;
 
-onFileSelected(event: Event): void {
-  const input = event.target as HTMLInputElement;
+AccountForm = new FormGroup({});
 
-  if (input.files && input.files[0]) {
-    const file = input.files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.uploadedImage = reader.result;
-      // Here you would typically send the file to the server
-      // this.uploadImageToServer(file);
-    };
-    reader.readAsDataURL(file);
-  }
-}
+// onFileSelected(event: Event): void {
+//   const input = event.target as HTMLInputElement;
+
+//   if (input.files && input.files[0]) {
+//     const file = input.files[0];
+//     const reader = new FileReader();
+//     reader.onload = (e) => {
+//       this.uploadedImage = reader.result;
+//       // Here you would typically send the file to the server
+//       // this.uploadImageToServer(file);
+//     };
+//     reader.readAsDataURL(file);
+//   }
+// }
 
   constructor(
-    private users: CustomerService
+    private users: CustomerService,
+    private http: HttpClient,
+    private route: Router
   ){}
   
 
@@ -47,6 +54,35 @@ onFileSelected(event: Event): void {
       this.user = result;
       console.log(this.user);
     })
+  }
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.files[0];
+    this.filename = this.selectedFile.name;
+  }
+
+  onPost(){
+    const fd = new FormData();
+    if(this.filename != null){
+      console.log(this.selectedFile);
+      fd.append('files',this.selectedFile);
+      fd.append('uid',this.userID);
+      // console.log(fd);
+      // console.log(this.bid);
+      fd.forEach((value,key)=>{
+        console.log(key + " " + JSON.stringify(value))
+      });
+
+      this.http.post('http://localhost/customer/saveaccountpic.php', fd, {observe: 'events', reportProgress: true}).subscribe((event: any)=>{
+        console.log(event);
+        if(event){
+          this.users.getuser(this.userID).subscribe((result: any)=>{
+            this.user = result;
+            console.log(this.user);
+          })
+        }
+      })
+    }
   }
 
 }
